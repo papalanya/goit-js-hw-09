6a23fe0923bc3cb036719175d91dca90a488dd7e
Illya Pavlenko <illiampavlenko@gmail.com>
2022-11-05 20:38:53 +0200

    add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const TIME_DELAY = 1000;
+let interval = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    startBtn.disabled = true;
+    interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, TIME_DELAY);
+}
+
+function onStopBtnClick() {
+    clearTimeout(interval);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
