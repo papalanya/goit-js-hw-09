7a20190728aaa8ed16d18721bc04ccb6ecbe1d2b
Illya Pavlenko <illiampavlenko@gmail.com>
2022-11-06 23:35:53 +0200

    add task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import Notiflix from "notiflix";
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+};
+
+
+refs.form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let {delay, step, amount} = onGettingData();
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({position, delay}) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({position, delay}) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      delay += step;
+  }
+});
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+  
+      if (shouldResolve) {
+        resolve({position: position, delay: delay});
+      } else {
+        reject({position: position, delay: delay});
+      }
+    }, delay);
+  })
+}
+
+
+function onGettingData() {
+  return {
+    delay: Number(refs.delay.value),
+    step: Number(refs.step.value),
+    amount: Number(refs.amount.value),
   }
 }
